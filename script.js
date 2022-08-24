25a6d961d490ff1d2c1d36444b01c8cfd4001cd8
alex <alex.achramowicz@gmail.com>
2022-08-23 20:24:33 -0700

    updated operand, operator, equals functionality

--- script.js
+++ script.js
@@ -14,36 +14,78 @@ function divide(a,b) {
     return a/b;
 }
 
-function sqrt(a,b) {
+// function sqrt(a) {
+//     return Math.sqrt(a);
+// }
 
-}
+// function remainder(a,b) {
+//     return a%b;
+// }
 
-function operate(operator,a,b) {
+function unaryOperate(operator, a) {
     let value;
-    // check for Number()?
+
     switch(operator) {
-        case add:
-            value = add(a,b);
-        case subtract:
-            value = subtract(a,b);
-        case multiply:
-            value =multiply(a,b);
-        case divide:
-            value =divide(a,b);
+        case sqrt:
+            value = sqrt(a);
         default:
             value = 0;
     }
+    return value;
+}
 
+
+function binaryOperate(operator,a,b) {
+    let value;
+    // check for Number()?
+    switch(operator) {
+        case 'add':
+            value = add(a,b);
+            console.log(value);
+            break;
+        case 'subtract':
+            value = subtract(a,b);
+            break;
+        case 'multiply':
+            value = multiply(a,b);
+            break;
+        case 'divide':
+            value = divide(a,b);
+            break;
+        default:
+            value = 0;
+    }
+    return value;
 }
 
 const calcContainer = document.querySelector('.calculator-container');
 const operandButtons = document.querySelectorAll('.operand-btn');
+const displayContent = document.querySelector('#display');
+const operatorButtons = document.querySelectorAll('.operator-btn');
+const equalsButton = document.querySelector('#equals-btn')
+const clearButton = document.querySelector('#clear-btn');
+
+let firstOperand, secondOperand, operator;
 
 operandButtons.forEach(operandButton => { 
     operandButton.addEventListener('click', () => { 
-        console.log(operandButton.textContent);
+        if(!firstOperand) {
+            firstOperand = parseInt(operandButton.value);
+        } else {
+            secondOperand = parseInt(operandButton.value);
+        }
+        displayContent.textContent = operandButton.value;
     });
 });
 
+operatorButtons.forEach(operatorButton => {
+    operatorButton.addEventListener('click', () => {
+        operator = operatorButton.value;
+    });
+});
 
-const operatorButton = document.querySelectorAll('.operator-btn');
+equalsButton.addEventListener('click', () => {
+    const value = binaryOperate(operator, firstOperand, secondOperand);
+    console.log(firstOperand + operator + secondOperand + '=' + value);
+    displayContent.textContent = value;
+})
